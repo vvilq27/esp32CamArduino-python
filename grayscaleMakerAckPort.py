e7955f22c05d4013ebbcc1ea4ae86ebc6518451c
unknown <vvilq27@gmail.com>
2022-10-06 15:05:20 +0200

    code sweeping

--- grayscaleMakerAckPort.py
+++ grayscaleMakerAckPort.py
@@ -19,51 +19,47 @@ class ImgRow:
 	def __str__(self):
 		return "[" + imgIdx + "-" + imgRowIdx + "] " + data[:10]
 
-def checkNewImage(packet):
-	if packet[:7] == 'newimg:':
-		return int(packet[7:-2])
-	else:
+def validateLine(line):
+	if not (line[3] == 44 and line[7] == 124):
+		print("invalid line detected: ", end="")
+		print(line)
+		
 		return False
-
-def waitForNewImage():
-	ser.reset_input_buffer()
-	while True:
-		packet = ser.readline()
-
-		if len(packet)>20:
-			continue
-		else:
-			packet = packet.decode('utf-8')
-			newImgLength = checkNewImage(packet)
-			
-			if newImgLength:
-				return newImgLength
+	else:
+		return True
 
 def getRowFromMcu(rowNumber):
 	ser.write(bytes(str(n), 'utf-8'))
-	time.sleep(0.15)
-	row = ser.readline()[:-2]
-	# print(len(row) , end=' ')
-	# print(row)
 
+	delay = 0.05
+
+	time.sleep(delay)
+
+	while ser.in_waiting != 102:
+		ser.reset_input_buffer()
+		delay += 0.01
+		print("getRowFromMcu | delay increased")
+
+		ser.write(bytes(str(n), 'utf-8'))
+
+	row = ser.read(ser.in_waiting)[:-2]
+	
 	return row
+
+
+
+# ==============================================
+# ==============================================
+# ==============================================
+# ==============================================
 
 
 ser = serial.Serial('COM3', 1000000, timeout=0.01 )#, parity=serial.PARITY_EVEN, rtscts=1)
 ser.set_buffer_size(rx_size = 100000, tx_size = 256)
 
-
-# bytesCountToRead = waitForNewImage()
-
-# print(bytesCountToRead)
-
 ser.reset_input_buffer()
 
 missingRowNumbers = list(range(768))
-
-# missingRowNumbers.remove(1)
-# missingRowNumbers.remove(5)
-# print(missingRowNumbers)
 
 rows = dict()
 rws = []
@@ -75,31 +71,22 @@ rws = []
 	# print(ser.in_waiting)
 
 ser.write(b'ok')
-time.sleep(1.5)
-
-
+time.sleep(1)
+print(ser.in_waiting)
 print("pic collected")
 
 while ser.in_waiting != 0:
 	line = ser.readline()
-	line = line[:-2]
+	line = line[:-2]	
 
-	# lineValidityCheck = re.match("\d\d\d,\d\d\d\|", line)
-
-	# if lineValidityCheck == None:
-	# 	print("invalid line detected: ", end="")
-	# 	print(line)
-	# 	continue		
-
-	if len(line) < 40:
+	if len(line) != 108:
 		continue
 
+# check for "," and "|" separators
+	lineValid = validateLine(line)
 
-	if not (line[3] == 44 and line[7] == 124):
-		print("invalid line detected: ", end="")
-		print(line)
+	if not lineValid:
 		continue
-
 
 	data = line[8:]
 	# actually first index is not needed in that mode
@@ -116,29 +103,18 @@ while ser.in_waiting != 0:
 	except:
 		continue
 
-
 	try:
 		missingRowNumbers.remove(imgRowIdx)
 	except:
 		print("cannot remove number: ", end = "")
 		print(imgRowIdx)
 
-	# row = ImgRow(imgIdx, imgRowIdx, data)
-
-	# print(len(data), end = ' ')
-	# print(row)
-
 	rows[imgRowIdx] = data
 
 print(missingRowNumbers)
 
-# for l in rows:
-# 	print(l, end='. ')
-# 	print(rows[l])
-
-
 for n in missingRowNumbers:
-	print(n, end = ' is missing, retry')
+	print(n, end = ' is missing, retry\n')
 	row = getRowFromMcu(n)
 
 	while len(row) != 100:
@@ -152,82 +128,8 @@ result = bytearray()
 
 for l in collections.OrderedDict(sorted(rows.items())):
 	result += rows[l]
-	# print(l, end='. ')
-	# print(len(rows[l]))
-	# print(rows[l])
-
-print(len(result))
 
 makeGrayImg(result)
 
 
-time.sleep(5)
-
-
-
-# for i in range(9):
-# 	data = ser.readline()
-# 	print(data)
-
-
-while True:
-
-	# for i in range(9):
-	# 	ser.write(hex(i))
-	# 	data = ser.readline()
-	# 	print(data)
-
-	# time.sleep(1)
-	break
-
-	# imgBytesArr = []
-
-	# idx = 50
-
-	# while idx:
-	# 	line = ser.read(4000)
-	# 	imgBytesArr.append(line)
-	# 	idx = idx -1
-	# 	# print(idx)
-
-	# nlFlag = False
-
-	# # for line in imgBytesArr:
-	# for c in data:
-	# 	if c=='\r':
-	# 		nlFlag = True
-	# 		continue
-
-	# 	if c == '\n' and nlFlag:
-	# 		print()
-	# 		nlFlag = False
-
-	# 	nlFlag = False
-
-	# 	try:
-	# 		print(chr(c), end='')
-	# 	except:
-	# 		print("", end='')
-
-	
-
-	# input("Press Enter to continue...")
-
-	'''
-	print('pic done')
-
-	randomByteArray = bytearray(data)
-	flatNumpyArray = numpy.array(randomByteArray)
-
-
-	grayImage = flatNumpyArray.reshape(120*2, 160*2)
-	grayImage = grayImage.transpose()
-	grayImage = numpy.flip(grayImage,1)
-
-	# cv2.imwrite('RandomGray.png', grayImage)
-
-	displayImg(grayImage)
-
-	ser.readline()
-	ser.readline()
-	'''
+time.sleep(3)
